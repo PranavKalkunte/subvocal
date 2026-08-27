01b37c2d6651e9521ee0e3fa712351b3468c2593
Pranav Kalkunte <pranav.kalkunte@gmail.com>
2026-08-27 16:04:29 -0500

    chore: bump version to 2.1.0

--- src/subvocal/__init__.py
+++ src/subvocal/__init__.py
@@ -33,7 +33,7 @@ from .exceptions import (
 from .hardware.brainflow_compat import BoardIds, BoardShim, BrainFlowInputParams
 from .paths import get_data_dir, get_models_dir
 
-__version__ = "2.0.1"
+__version__ = "2.1.0"
 
 logging.getLogger(__name__).addHandler(logging.NullHandler())
 
